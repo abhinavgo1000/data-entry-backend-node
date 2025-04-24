032ce923098941f6f74d176b36ff3f55c9b60793
Abhinav Goel <abhinavgo1000@gmail.com>
2025-04-24 10:37:13 -0600

    link attribute added

--- models/AboutMe.js
+++ models/AboutMe.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
+const { link } = require('..');
 
 const AboutMeSchema = new mongoose.Schema({
-    message: { type: String, required: true }, // Example field for the string content
+    message: { type: String, required: true },
+    link: { type: String, required: true }
 });
 
 module.exports = mongoose.model('AboutMe', AboutMeSchema, 'aboutme'); // 'aboutme' is the name of the collection in MongoDB
